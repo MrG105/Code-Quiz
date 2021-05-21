090628fda439ed30ff3b32179d909487861ee929
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-05-21 03:50:18 -0500

    Added todo list for tomorrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,5 +24,19 @@ function startTimer() {
     }, 1000);
 }
 
+// TODO: Clear container text elements on button press
+// Start Quiz with questions 
+
+// Local Storage: High Scores
+
+// Quiz Questions: 
+// Commonly used data types DO NOT include: strings, booleans, *alerts*, numbers
+// The condition in an if / else statement is enclosed within: quotes, *curly brackets*, paranthesis, square brackets
+// Arrays in JavaScript can be used to store: numbers and strings, other arrays, booleans, *all of the above*
+// String values must be enclosed within ___ when being assigned to variables: commas, curly brackets, *quotes*, paranthesis 
+// A useful tool used during development and debugging for printing content to the debugger is: JavaScript, terminal/bash, for loops, *console.log*
+
+
+
 // Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startTimer);
