a7394c6a0c33e1561b270c7ecdd72e22fa1cf97d
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-05-22 19:00:17 -0500

    added high score functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,19 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var button4 = document.getElementById("button4");
-var rightOrWrong = document.getElementById("rightOrWrong")
+var submitButton = document.getElementById("submit");
+var rightOrWrong = document.getElementById("rightOrWrong");
+var h1 = document.getElementById("h1");
+var timeLeft= 60;
+var timeInterval;
+var inputGroup = document.getElementById("input-group");
+var initialsInput = document.querySelector("#initials");
+var highScoresCard = document.querySelector(".highScores")
 
 // Question and Answer Arrays
 var question = document.getElementById("questions");
-var correctAnswers = ["alerts", "curly brackets", "all of the above", "quotes","console.log"]
+var correctAnswers = ["alerts", "parentheses", "all of the above", "quotes","console log"]
 var currentQuestionIndex = 0;
-var timeLeft= 60;
-
 var questions = [
     {
         "question": "Commonly used data types DO NOT include:", 
@@ -33,23 +38,24 @@ var questions = [
     },
     {
         "question": "A useful tool used during development and debugging for printing content to the debugger is:", 
-        "answers": ["JavaScript","terminal/bash","for loops","console.log"]
+        "answers": ["JavaScript","terminal/bash","for loops","console log"]
     }
 ]
 
 // // The startTimer function starts and stops the timer
 function startTimer() {
     
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
         if (timeLeft > 1) {
             timerEl.textContent = timeLeft + ' seconds remaining';
             timeLeft--;
         } else if (timeLeft === 1) {
             timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
         } else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
-            displayMessage();
+            endGame();
         }
     }, 1000);
 }
@@ -59,18 +65,21 @@ function subtractTime() {
     timeLeft -=10;
 }
 
-
 // TODO: 
 // Local Storage: High Scores
-// function of losing: call them a bitch
-// restart button
 
 function startGame() {
+    h1.style.display = "none";
+    timeLeft = 60;
+    currentQuestionIndex = 0;
     startTimer();
     startButton.style.display= "none";
     showQuestions();
     questioncontainer.style.display ="block";
-    
+    question.style.display = "block";
+    inputGroup.style.display = "none";
+    highScoresCard.style.display = "none";
+
 }
 
 function fillAnswers() {
@@ -80,10 +89,13 @@ function fillAnswers() {
     button4.textContent = questions[currentQuestionIndex].answers[3];
 }
 
-
 function showQuestions() {
+    if (currentQuestionIndex > 4) {
+        endGame();
+    }else {    
     question.textContent = questions[currentQuestionIndex].question
     fillAnswers();
+    }
 }
 
 // check for correct/incorrect click
@@ -109,10 +121,43 @@ function checkAnswer(event) {
     showQuestions()
 }
 
-// Attach event listener to start button to call startGame function on click
+function endGame() {
+    h1.style.display = "block";
+    clearInterval(timeInterval);
+    if (timeLeft>=1) {
+        h1.textContent = "YOU WIN"
+        question.textContent = "Enter your initials to save your high score!"
+        inputGroup.style.display = "block";
+    } else {
+        h1.textContent = "TIME'S UP!"
+        question.style.display = "none";
+    } 
+    questioncontainer.style.display = "none";
+    startButton.style.display = "block";
+    startButton.innerText = "Restart Quiz?";
+    highScoresCard.style.display = "block";
+    
+}
+
+function init() {
+    var storedHighScores = JSON.parse(localStorage.getItem("highScores"));
+    if (storedHighScores !== null) {
+        highScores = storedHighScores;
+    }
+}
+// Attach event listener to start button to call startGame function on click, checkAnswer function on quiz buttons
 startButton.addEventListener("click", startGame);
 button1.addEventListener("click", checkAnswer);
 button2.addEventListener("click", checkAnswer);
 button3.addEventListener("click", checkAnswer);
 button4.addEventListener("click", checkAnswer);
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    var highScores = {
+        initials: initialsInput.value,
+        score: timeLeft
+    };
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+})
 
+init()
